236502e72a607dcbfe7bcfd0ad23d8d3f0815253
zaid alhakeem <zaidalhakeem86@gmail.com>
2023-09-27 21:42:36 -0500

    ASPwork

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import { Header, Nav, Main, Footer } from "./components";
 
-function render() {
-  document.querySelector(
-    "#root"
-  ).innerHTML = `${Header()} ${Nav()} ${Main()} ${Footer()}`;
+import * as store from "./store";
+import Navigo from "navigo";
+import { capitalize } from "lodash";
+
+const router = new Navigo("/");
+
+function render(state = store.Home) {
+  document.querySelector("#root").innerHTML = `${Header(state)} ${Nav(
+    store.Links
+  )} ${Main(state)} ${Footer()}`;
   afterRender();
+  router.updatePageLinks();
 }
 
 function afterRender() {
@@ -14,4 +21,17 @@ function afterRender() {
   });
 }
 
-render();
+router
+  .on({
+    "/": () => render(),
+    ":view": params => {
+      let view = capitalize(params.data.view);
+      if (view in store) {
+        render(store[view]);
+      } else {
+        render(store.Viewnotfound);
+        console.log(`View ${view} not defined`);
+      }
+    }
+  })
+  .resolve();
