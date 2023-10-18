67e73d3f6e0949b5f4b94a72a8d1e9c32bf7c1bb
zaid alhakeem <zaidalhakeem86@gmail.com>
2023-10-17 21:22:46 -0500

    changing the key and onther satf

--- server/app.js
+++ server/app.js
@@ -1,7 +1,55 @@
 // 'Import' the Express module instead of http
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import pizzas from "./routers/pizzas.js";
+
 // Initialize the Express application
 const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB, {
+  // Configuration options to remove deprecation warnings, just include them to remove clutter
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
+const PORT = process.env.PORT || 4040;
+
+const logging = (request, response, next) => {
+  console.log(
+    `${request.method} ${request.url} ${new Date().toLocaleString("en-us")}`
+  );
+  next();
+};
+
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
+app.use(cors);
+app.use(express.json());
+app.use(logging);
 
 // Handle the request with HTTP GET method from http://localhost:4040/status
 app.get("/status", (request, response) => {
@@ -48,6 +96,8 @@ app.get("/weather/:city", (request, response) => {
   });
 });
 
+app.use("/pizzas", pizzas);
+
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
-app.listen(4040, () => console.log("Listening on port 4040"));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
